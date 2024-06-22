94ce0273b063bccfd8fe73982236a3dfd8e616bb
Fluffion <127639820+Fluffion@users.noreply.github.com>
2024-06-22 19:33:48 +0100

    added lil splash text, changed background, removed pronouns

--- script.js
+++ script.js
@@ -28,3 +28,32 @@ function startTime() {
   }
   
   startTime()
+
+  var textArray = [
+  ":3",
+  "yello",
+  "haii",
+  "yo waddup",
+  "listen to Lemon Demon",
+  "fortnite",
+  "eating Burger King",
+  "paws",
+  "definitely a cat",
+  "definitely a fox",
+  "meow",
+  "hungry",
+  "undefined",
+  "defined!",
+  "creature of secrets",
+  "anybody got $12.76",
+  "kinda want McDonald's",
+  "could really go for some McDonald's",
+  "fat free",
+  "no eta",
+  "play Rain World!",
+  "woah",
+  "whoa",
+  
+  ];
+  var randomNumber = Math.floor(Math.random() * textArray.length);
+  document.getElementById("randomNumber").innerHTML = textArray[randomNumber];
