2f31225150331ff69eb02f51fa4ceec1747a5c77
Fluffion <127639820+Fluffion@users.noreply.github.com>
2024-11-02 04:23:08 +0000

    updated favicon and added funny splashes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+  var titles = ['the big red thing’s site', '<-- slugcat fella', 'T.W.O.A.T.', 'psst, play Rain World', 'the site'];
+  var random = Math.floor(Math.random() * titles.length);
+  var titleElement = document.getElementById('title')
+  titleElement.innerHTML = titles[random];
+}
+  
   var textArray = [
   ":3",
   "yello",
@@ -16,19 +23,25 @@
   "hungry",
   "undefined",
   "defined!",
+  "'an unimaginable gift.'",
+  "'an unimaginable curse.'",
   "porls...",
   "gadzooks!",
   "you should click that big title there ^",
   "meow",
   "bark",
   "say it ain't so!",
+  "'this moment, right here! it is where we are meant to be.'",
+  "'swim with the tide or against it.'",
   "creature of secrets",
   "anybody got $12.76",
   "kinda want McDonald's",
   "'how strange it is to be anything at all'",
   "could really go for some McDonald's",
+  "these fishes gay. good for them, good for them.",
   "fat free",
-  "not fat free",
+  "contains trans fats",
+  "'the irony is not lost on me, mouse!'",
   "no eta",
   "play Rain World!",
   "woah",
